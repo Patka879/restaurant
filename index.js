65423a1b8d81c6dd2d6907d9ac095101b182c541
mysiak.patrycja@gmail.com <mysiak.patrycja@gmail.com>
2022-07-15 20:51:36 +0200

    media query applied to differen breakpoints

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 "use strict"
 
-import Swiper from "swiper"
-import 'swiper/css'
+// import Swiper from "swiper"
+// import 'swiper/css'
 
-const swiper = new Swiper
+// const swiper = new Swiper
+
+
+document.querySelector(".dropdown-menu-btn").addEventListener("click", function() {
+    let nav = document.querySelector(".nav-top")
+    if (nav.style.display === "flex") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "flex";
+    }
+    console.log("clidked")
+  }
+)
+
+    
